038226c88e20d0c903db4bf15ad03724540146ab
francis-codex <francislogo2219@gmail.com>
2024-01-16 19:32:46 +0100

    template faster logistics

---
+++ classwork/classwork.js
@@ -0,0 +1,15 @@
+input = document.createElement('input')
+input.setAttribute('type', 'password')
+
+btn = document.createElement('button')
+btn.textContent='show'
+
+document.body.appendChild(input)
+document.body.appendChild(btn)
+
+// btn.onclick = () => {
+//     console.log(btn)
+//     if(btn.textContent == 'show')
+//     input.setAttrubute('type', 'password')
+//     btn.textContent = 
+// }
